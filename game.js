579bb19a6f65520bc02a119ee6984ad0e3e75f7c
LynneMartin <lynnemartin@gmail.com>
2019-06-17 13:21:50 -0600

    pmo

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 
-let maxHealth = 100;
-let health = maxHealth;
+let health = 100;
 let hits = 0;
 let name = "The Plastics"
 let targetHealth = document.getElementById("health")
+let hitCount = document.getElementById("hits")
+let charItems = []
 
 // individual targets
 // let meanGirl = {
@@ -22,45 +23,63 @@ let targetHealth = document.getElementById("health")
 //  }
 //}],
 
-
+function update() {
+  // conditionals here to check if health is below 0 or greater than 100
+  targetHealth.innerText = health.toString();
+  hitCount.innerText = hits.toString();
+}
 
 // health damage count on attack click
 function hairPull() {
-  health -= 1;
-  hits += 1; // **not working**
-  targetHealth.innerText = health.toString();
+  health -= 1 // + addMods()
+  hits += 1;
+  update()
 }
 
 function scratch() {
   health -= 5;
-  hits += 1; // **not working**
-  targetHealth.innerText = health.toString()
+  hits += 1;
+  update()
 }
 
 function boobPunch() {
   health -= 10;
-  hits += 1; // **not working**
-  targetHealth.innerText = health.toString();
+  hits += 1;
+  update();
 }
 
 // power up
 function humpDayTreat() {
   health += 10
-  targetHealth.innerText = health.toString();
+  update();
 }
 
-// reset hit count to 0 and health to 100 ***NOT WORKING***
+// reset hit count to 0 and health to 100
 function reset() {
   health = 100;
   hits = 0;
   update();
 }
 
-// ***NOT UNDERSTANDING ITEM/MODIFIER REQUIREMENT***
-//let items = {
-//item: { name: '', modifier: 2, description: '' }
-//item: { name: '', modifier: 4, description: '' }
-//item: { name: '', modifier: 6, description: '' }
-//}
 
-update();
+let items = {
+  proteinBar: { name: 'Kälteen Protein Bar', modifier: 2, description: 'Tasty, but makes target gain weight quickly.' },
+  hairspray: { name: 'Hairspray In Eyes', modifier: 4, description: 'It burns!' },
+  purse: { name: 'Purse Thwap', modifier: 6, description: 'Makes target see stars.' }
+}
+
+function addItem(itemName) {
+  // add the item from the items dictionary above with the itemName to the charItems array
+  charItems.push(items[itemName])
+}
+
+function addMods() {
+  // create a modifierTotal variable set to 0
+  // loop over the charItems array and for each element in the array
+  // add the modifier value of the item to the modifierTotal variable
+  //return the modifierTotal
+}
+
+
+
+// update();
